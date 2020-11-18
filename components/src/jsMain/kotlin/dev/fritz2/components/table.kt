50f845e99dd0a9d6c8eb094e74b6a33e6331fae2
WJA <jan.westphal-huusmann@oeffentliche.de>
2020-11-18 13:43:39 +0100

    table

--- components/src/jsMain/kotlin/dev/fritz2/components/table.kt
+++ components/src/jsMain/kotlin/dev/fritz2/components/table.kt
@@ -3,6 +3,8 @@ package dev.fritz2.components
 import dev.fritz2.binding.*
 import dev.fritz2.dom.html.RenderContext
 import dev.fritz2.dom.html.Td
+import dev.fritz2.dom.html.Th
+import dev.fritz2.dom.html.render
 import dev.fritz2.dom.states
 import dev.fritz2.identification.uniqueId
 import dev.fritz2.lenses.Lens
@@ -15,20 +17,81 @@ import dev.fritz2.styling.theme.Property
 import dev.fritz2.styling.theme.theme
 import kotlinx.coroutines.flow.*
 
-
+/**
+ * TODO open questions
+ *  tfoot what will we do with this section of a table?
+ *
+ * TODO open todos
+ *
+ */
 class TableComponent <T>{
     companion object {
         const val prefix = "table"
         val staticCss = staticStyle(
             prefix,
             """
+                display:grid;
+                //min-width: 100vw;
+                width: auto;
+                flex: 1;
+                display: grid;
+                border-collapse: collapse;
                 text-align: left;
+                
+                thead,tbody,tr {
+                    display:contents;
+                }
+                             
+                thead {             
+                  grid-area:main;  
+                }
+                
+                tbody {             
+                  grid-area:main;  
+                }
+                                
+                th,
+                td {
+                  padding: 0.5rem;
+                  overflow: hidden;
+                  text-overflow: ellipsis;
+                  white-space: nowrap;
+                  &:last-child {
+                    border-right: none;
+                  }
+                }
+                
+                td {
+                    border-bottom: 1px solid inherit;
+                }
+                
+                th {
+                  position: sticky;
+                  top: 0;
+                  background: rgb(52, 58, 64);
+                  text-align: left;
+                  font-weight: normal;
+                  font-size: 1.1rem;
+                  color: rgb(226, 232, 240);
+                  position: relative;
+                }
+                
+                tr {
+                    td { background: rgba(52, 58, 64, 0.1); }
+                    &:nth-child(even) {
+                        td {  background: rgba(52, 58, 64, 0.2); }
+                    } 
+                    &.selected {
+                    td { background: rgba(255, 193, 7, 0.8) }
+                    }
+                }
             """
         )
 
         enum class SelectionMode {
             NONE,
             SINGLE,
+            SINGLE_CHECKBOX,
             MULTI
         }
 
@@ -37,11 +100,18 @@ class TableComponent <T>{
             ASC,
             DESC
         }
+
+        enum class CaptionPlacement {
+            TOP,
+            BOTTOM
+        }
+
     }
+
 
     data class TableColumn<T>(
         val lens: Lens<T,String>? = null,
-        val headerName: String,
+        val headerName: String = "",
         val minWidth: Property? = null,
         val maxWidth: Property? = null,
         val displayPriority: String = "sm",
@@ -50,6 +120,10 @@ class TableComponent <T>{
         val sortDirection: SortDirection = SortDirection.NONE,
         val filter: Unit = Unit,
         val styling: Style<BasicParams> = {},
+        val stylingHead: Style<BasicParams> = {},
+        val contentHead : ( renderContext: Th, tableColumn: TableColumn<T> ) -> Unit = { renderContext, config ->
+            renderContext.apply{ +config.headerName }
+        },
         val content: (renderContext: Td,
                       cellStore: Store<String>?,
                       rowStore: SubStore<List<T>,List<T>,T>?) -> Unit  = { renderContext,store,_ ->
@@ -59,8 +133,14 @@ class TableComponent <T>{
                       }
     )
 
-    var selectionMode: SelectionMode = SelectionMode.NONE
+    var defaultMinWidth: Property = "150px"
+    var defaultMaxWidth: Property = "1fr"
+
+    var selectionMode: Flow<SelectionMode> = flowOf(SelectionMode.NONE)
     fun selectionMode(value: SelectionMode) {
+        selectionMode = flowOf(value)
+    }
+    fun selectionMode(value: Flow<SelectionMode>) {
         selectionMode = value
     }
 
@@ -72,7 +152,6 @@ class TableComponent <T>{
         configStore.update(value)
     }
 
-    //TODO - Store? Aktuell nicht möglich, wegen des benötigten ID Provider
     var tableStore: RootStore<List<T>> = storeOf(emptyList())
     fun tableStore(value: RootStore<List<T>>) {
         tableStore = value
@@ -85,6 +164,39 @@ class TableComponent <T>{
 
     var selectedRowEvent: SimpleHandler<T>? = null
     var selectedAllRowEvents:  SimpleHandler<List<T>>? = null
+
+
+    var captionPlacement: CaptionPlacement = CaptionPlacement.TOP
+    fun captionPlacement(value :CaptionPlacement)  {
+        captionPlacement = value
+    }
+
+    var caption: (RenderContext.() -> Unit)? = null
+    fun caption(value: (RenderContext.() -> Unit)) {
+        caption = {
+            (::caption.styled() {
+                if( captionPlacement == CaptionPlacement.TOP ) {
+                  css("grid-area:header;")
+                } else {
+                    css("grid-area:footer;")
+                }
+            }){ value() }
+        }
+    }
+    fun caption(value: String) {
+        this.caption(flowOf(value))
+    }
+    fun caption(value: Flow<String>) {
+        caption = {
+            (::caption.styled() {
+                if( captionPlacement == CaptionPlacement.TOP ) {
+                    css("grid-area:header;")
+                } else {
+                    css("grid-area:footer;")
+                }
+            }){ value.asText() }
+        }
+    }
 }
 
 fun <T, I> RenderContext.table(
@@ -97,69 +209,85 @@ fun <T, I> RenderContext.table(
 ) {
     val component = TableComponent<T>().apply(build)
 
-    val tableBaseClass = if( baseClass != null ) {
+    val tableBaseClass = if (baseClass != null) {
         baseClass + TableComponent.staticCss
     } else {
         TableComponent.staticCss
     }
+    component.selectionMode.watch()
 
-    val config = component.configStore.data.map { it.sortedBy { it.position } }
 
-
-    (::table.styled({
-        styling()
-    }, tableBaseClass ,id,prefix){}){
-        className(component.configStore.data.map {
-            staticStyle(
-                "myTable", {
-                    css("display:grid;")
-                    columns { repeat(it.size){ "1fr" } }
-                }
-            ).name
-        })
-
-        thead {
-            tr {
-                th {
-                    if (component.selectionMode == TableComponent.Companion.SelectionMode.MULTI) {
-                        checkbox( {display { inlineBlock }  }  ,id = uniqueId()) {
-                            text("")
-                            borderColor { theme().colors.secondary }
-                            checkedBackgroundColor { theme().colors.warning }
-                            events {
-                                component.selectedAllRowEvents?.let {
-                                    changes.states().combine(component.tableStore.data) { selected, list ->
-                                        if (selected) {
-                                            list
-                                        } else {
-                                            emptyList()
-                                        }
-                                    } handledBy it
+    val additionalCol = component.selectionMode.map { selectionMode ->
+        if (selectionMode == TableComponent.Companion.SelectionMode.MULTI) {
+            listOf(
+                TableComponent.TableColumn<T>(
+                    minWidth = "50px",
+                    maxWidth = "50px",
+                    contentHead = { ctx, _ ->
+                        ctx.apply {
+                            checkbox({ display { inlineBlock } }, id = uniqueId()) {
+                                text("")
+                                borderColor { theme().colors.secondary }
+                                checkedBackgroundColor { theme().colors.warning }
+                                events {
+                                    component.selectedAllRowEvents?.let {
+                                        changes.states().combine(component.tableStore.data) { selected, list ->
+                                            if (selected) {
+                                                list
+                                            } else {
+                                                emptyList()
+                                            }
+                                        } handledBy it
+                                    }
                                 }
                             }
                         }
-                    } else {
-                        span{}
-                    }
-                }
-
-                config.renderEach { ctx ->
-                    th {
-                        +ctx.headerName
-                    }
-                }
-            }
-        }
-        tbody {
-            component.tableStore.renderEach(rowIdProvider){ rowStore ->
-                tr {
-                    if ( component.selectionMode == TableComponent.Companion.SelectionMode.MULTI)  {
-                        td {
-                            checkbox({ display { inlineBlock }  },
-                                id = uniqueId()) {
+                    },
+                    content = { ctx, _, rowStore ->
+                        ctx.apply {
+                            checkbox(
+                                { display { inlineBlock } },
+                                id = uniqueId()
+                            ) {
                                 text("")
                                 checkedBackgroundColor { theme().colors.warning }
+                                if (rowStore != null) {
+                                    checked {
+
+                                        component.selectedRows.combine(rowStore.data) { selectedRows, thisRow ->
+                                            selectedRows.contains(thisRow)
+                                        }
+                                    }
+                                    events {
+                                        component.selectedRowEvent?.let {
+                                            clicks.events.combine(rowStore.data) { _, thisRow ->
+                                                thisRow
+                                            } handledBy it
+                                        }
+
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                )
+            )
+        } else if (selectionMode == TableComponent.Companion.SelectionMode.SINGLE_CHECKBOX) {
+            listOf(TableComponent.TableColumn<T>(
+                minWidth = "50px",
+                maxWidth = "50px",
+                content = { ctx, _, rowStore ->
+                    ctx.apply {
+                        checkbox(
+                            { display { inlineBlock } },
+                            id = uniqueId()
+                        ) {
+                            text("")
+                            checkedBackgroundColor { theme().colors.warning }
+                            if (rowStore != null) {
                                 checked {
+
                                     component.selectedRows.combine(rowStore.data) { selectedRows, thisRow ->
                                         selectedRows.contains(thisRow)
                                     }
@@ -173,16 +301,83 @@ fun <T, I> RenderContext.table(
 
                                 }
                             }
-                        }
-                    } else if( component.selectionMode == TableComponent.Companion.SelectionMode.SINGLE ) {
-                        component.selectedRowEvent?.let {
-                            clicks.events.combine(rowStore.data) { _, thisRow ->
-                                thisRow
-                            } handledBy it
+
                         }
                     }
+                }
+            ))
+        } else {
+            emptyList()
+        }
+    }
+    val config = additionalCol.combine(component.configStore.data.map { it.filterNot { it.hidden }.sortedBy { it.position } }){a, b -> a + b}
+
+    val gridCols = config.map { configItems ->
+        var minmax = ""
+        var header = ""
+        var footer = ""
+        var main = ""
+
+
+        configItems.forEach { item ->
+            val min = item.minWidth ?: component.defaultMinWidth
+            val max = item.maxWidth ?: component.defaultMaxWidth
+
+            minmax += "minmax($min, $max)"
+            main += "main "
+            footer += "footer "
+            header += "header "
+        }
+
+           """
+            grid-template-columns: $minmax;                
+            grid-template-rows: auto;
+            grid-template-areas:
+           "$header"
+           "$main"
+           "$footer";
+           """
+    }
+
+    (::table.styled({
+        styling()
+    }, tableBaseClass, id, prefix) {}){
+        attr("style", gridCols)
+        if (component.captionPlacement == TableComponent.Companion.CaptionPlacement.TOP) {
+            component.caption?.invoke(this)
+        }
+
+        thead {
+            tr {
+                config.renderEach { ctx ->
+                    (::th.styled(ctx.stylingHead){})  {
+                        ctx.contentHead(this, ctx)
+                    }
+                }
+            }
+        }
+        tbody {
+            component.tableStore.renderEach(rowIdProvider) { rowStore ->
+
+                val selected = component.selectedRows.combine(rowStore.data) { selectedRows, thisRow ->
+                        selectedRows.contains(thisRow)
+                }
+
+
+                tr {
+                    className( selected.combine( component.selectionMode ){ selected, selectionMode -> if(selected && selectionMode == TableComponent.Companion.SelectionMode.SINGLE){"selected"} else {""} })
+                    component.selectionMode.render { selectionMode ->
+                        if (selectionMode == TableComponent.Companion.SelectionMode.SINGLE) {
+                            component.selectedRowEvent?.let {
+                                clicks.events.combine(rowStore.data) { _, thisRow ->
+                                    thisRow
+                                } handledBy it
+                            }
+                        }
+                    }
+
                     config.renderEach { ctx ->
-                        td {
+                        (::td.styled(ctx.styling){}) {
                             if (ctx.lens != null) {
                                 val b = rowStore.sub(ctx.lens)
                                 ctx.content(this, b, rowStore)
@@ -191,8 +386,14 @@ fun <T, I> RenderContext.table(
                             }
                         }
                     }
+
                 }
             }
+
+            if (component.captionPlacement == TableComponent.Companion.CaptionPlacement.BOTTOM) {
+                component.caption?.invoke(this)
+            }
+
         }
     }
 }
--- components/src/jsMain/kotlin/tableDemo.kt
+++ components/src/jsMain/kotlin/tableDemo.kt
@@ -1,4 +1,5 @@
 import dev.fritz2.binding.RootStore
+import dev.fritz2.binding.storeOf
 import dev.fritz2.components.*
 import dev.fritz2.components.model.*
 import dev.fritz2.dom.html.Div
@@ -8,38 +9,64 @@ import dev.fritz2.lenses.Lens
 import dev.fritz2.lenses.buildLens
 import dev.fritz2.lenses.format
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
+import kotlin.js.Date
 
 val defaultList = listOf(
     Person(uniqueId(),
         1,
         "Mr. John Doe",
-        "Street",
-        "1",
+        "1970-01-01",
+        "john.doe@example.com",
         "13245",
-        "City",
-        Address("Street")
+        "8445645",
+        Address("Street","1","12345","City")
     ),
-    Person(uniqueId(), 2, "Mrs. Jane Doe", "Avenue", "42", "78945", "Hometown"),
-    Person(uniqueId(), 3, "Dr.  Jean Dupont", "Rue de bleu", "13", "56473", "Ville"),
-    Person(uniqueId(), 4, "Sir  Juan Pérez", "El Camino", "75", "85236", "Ciudad")
+    Person(uniqueId(),
+        2,
+        "Mrs. Jane Doe",
+        "2000-11-11",
+        "jane.doe@example.com",
+        "856715",
+        "84456 - 131 - 2345",
+        address =  Address("Avenue", "42", "78945", "Hometown")),
+    Person(uniqueId(),
+        3,
+        "Dr.  Jean Dupont",
+        "1990-09-01",
+        "jean.dupint@exemple.fr",
+        "986 - 4 45",
+        "84456 - 131 - 2345",
+        address = Address("Rue de bleu", "13", "FR - 56473", "Ville")),
+    Person(uniqueId(),
+        4,
+        "Sir  Juan Pérez",
+        "1980-12-01",
+        "juan.perez@ejemplo.es",
+        "897 / 123",
+        "+45 2345",
+        address = Address("El Camino", "75", "85236", "Ciudad"))
 )
 
 
-val _idLense = buildLens("_id", Person::_id) { p, v -> p.copy(_id = v) }
-val personIdLense = buildLens("id", Person::id) { p, v -> p.copy(id = v) }
-val fullNameLense = buildLens("fullName", Person::fullName) { p, v -> p.copy(fullName = v) }
+val _idLens = buildLens("_id", Person::_id) { p, v -> p.copy(_id = v) }
+val personIdLens = buildLens("id", Person::id) { p, v -> p.copy(id = v) }
+val fullNameLens = buildLens("fullName", Person::fullName) { p, v -> p.copy(fullName = v) }
+val birthdayLens = buildLens("birthday", Person::birthday) { p, v -> p.copy(birthday = v) }
+val emailLens = buildLens("email", Person::email) { p, v -> p.copy(email = v) }
+val mobileLens = buildLens("mobile", Person::mobile) { p, v -> p.copy(mobile = v) }
+val phoneLens = buildLens("phone", Person::phone) { p, v -> p.copy(phone = v) }
+
+
 val personAddressLens = buildLens("address", Person::address) { p, v -> p.copy(address = v) }
-val streetLense = buildLens("street", Address::street) { p, v -> p.copy(street = v) }
-/*
-val houseNumberLense = buildLens("houseNumber", Person::houseNumber) { p, v -> p.copy(houseNumber = v) }
-val postalCodeLense = buildLens("postalCode", Person::postalCode) { p, v -> p.copy(postalCode = v) }
-val cityLense = buildLens("city", Person::city) { p, v -> p.copy(city = v) }
-*/
+val streetLens = buildLens("street", Address::street) { p, v -> p.copy(street = v) }
+val houseNumberLens = buildLens("houseNumber", Address::houseNumber) { p, v -> p.copy(houseNumber = v) }
+val postalCodeLens = buildLens("postalCode", Address::postalCode) { p, v -> p.copy(postalCode = v) }
+val cityLens = buildLens("city", Address::city) { p, v -> p.copy(city = v) }
+
 
 object TableStore : RootStore<List<Person>>(defaultList, "personData") {
-
-
     val remove = handle<Person> {
             list, toDelete ->
         list.filter { it != toDelete }
@@ -57,12 +84,10 @@ object Formats {
 @ExperimentalCoroutinesApi
 fun RenderContext.tableDemo(): Div {
 
-    return div {
-        flexBox({
-            direction { column }
-            padding { normal }
-        }) {
-            h1 { +"Table Showcase" }
+
+
+
+    return div { h1 { +"Table Showcase" }
 
             val selectedStore = object: RootStore<List<Person>>(emptyList()){
 
@@ -89,59 +114,92 @@ fun RenderContext.tableDemo(): Div {
 
             }
 
-            p{
-                selectedStore.data.map {
-                    "Aktuell sind " + it.count() + " Zeilen ausgewählt!"
-                }
+           val selectionModeStore = storeOf(TableComponent.Companion.SelectionMode.NONE)
+           lineUp {
+               items {
+                   TableComponent.Companion.SelectionMode.values().toList().map { mode ->
+                       clickButton { text(mode.toString()) }.events.map{mode} handledBy selectionModeStore.update
+                   }
+               }
+           }
+
+
+            selectedStore.data.render { list ->
+                p{ + "Aktuell sind ${list.size} Zeilen ausgewählt!"}
             }
 
-            h2{+"myTable"}
+
+
             table<Person,Int>( rowIdProvider = Person::id ) {
-                selectionMode(TableComponent.Companion.SelectionMode.MULTI)
-                configStore(listOf(
-                    TableComponent.TableColumn(
-                        personIdLense + Formats.intFormat,
-                        "ID"
-                    ),
-                    TableComponent.TableColumn(
-                        fullNameLense,
-                        "Name"
-                    ),
-                    TableComponent.TableColumn(
-                        personAddressLens + streetLense,
-                        "editable Street",
-                        content = { ctx,store,_ ->
-                            ctx.apply{
-                                inputField(store = store) {  }
+                    caption(  selectionModeStore.data.map { mode ->
+                        "Table with \"${mode.name}\" Selection Mode "
+                    })
+                    tableStore(TableStore)
+                    selectedRows(selectedStore.data)
+                    selectedAllRowEvents = selectedStore.update
+                    selectedRowEvent = selectedStore.toggle
+                    selectionMode(selectionModeStore.data)
+                    configStore(listOf(
+                        TableComponent.TableColumn(
+                            personIdLens + Formats.intFormat,
+                            "ID",
+                            minWidth = "80px",
+                            maxWidth = "80px"
+                        ),
+                        TableComponent.TableColumn(
+                            fullNameLens,
+                            "Name",
+                            maxWidth = "1.33fr"
+                        ),
+                        TableComponent.TableColumn(
+                            birthdayLens,
+                            "Birthday",
+                            minWidth = "100px",
+                            maxWidth = "100px",
+                            styling = {
+                                color { danger }
+                            },
+                            stylingHead = {
+                                css("color:orange !important;")
                             }
-                        }
-                    ),
-                    TableComponent.TableColumn(
-                        personAddressLens + streetLense,
-                        "Street"
-                    ),
-                    TableComponent.TableColumn(
-                        null,
-                        "Actions",
-                        position = -1,
-                        content = {ctx, col, row ->
-                            ctx.apply {
-                                clickButton {
-                                    text("clear list of selected")
-                                } handledBy selectedStore.clearList
+                        ),
+                        TableComponent.TableColumn(
+                            null,
+                            "Address",
+                            maxWidth = "2fr",
+                            content = { ctx,_, rowStore ->
+                                rowStore?.let{ person ->
+                                    val street =  person.sub(personAddressLens + streetLens)
+                                    val houseNumber =  person.sub(personAddressLens + houseNumberLens)
+                                    val postalCode =  person.sub(personAddressLens + postalCodeLens)
+                                    val city =  person.sub(personAddressLens + cityLens)
+                                    ctx.apply {
+                                        street.data.combine(houseNumber.data) { s, h ->
+                                            "$s $h"
+                                        }.combine(postalCode.data) { a, p ->
+                                            "$a ,$p"
+                                        }.combine(city.data) { a, c ->
+                                            "$a $c"
+                                        }.asText()
+                                    }
+
+                                }
+
                             }
-                        }
-                    )
-
-
-                ))
-                tableStore(TableStore)
-                selectedRows(selectedStore.data)
-
-                selectedAllRowEvents = selectedStore.update
-                selectedRowEvent = selectedStore.toggle
-
-            }
+                        ),
+                        TableComponent.TableColumn(
+                            phoneLens,
+                            "Phone"
+                        ),
+                        TableComponent.TableColumn(
+                            mobileLens,
+                            "Mobile"
+                        ),
+                        TableComponent.TableColumn(
+                            emailLens,
+                            "eMail"
+                        ),
+                    ))
+                }
          }
-    }
 }
